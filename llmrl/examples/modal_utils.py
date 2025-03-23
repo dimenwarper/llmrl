f56c0d513039e3060170c7966624c6aadb24688c
Pablo Cordero <dimenwarper@gmail.com>
2025-03-22 22:25:49 -0700

    prepping to run in modal

---
+++ llmrl/examples/modal_utils.py
@@ -0,0 +1,32 @@
+import tomli
+import modal
+
+def create_modal_image_from_pyproject(pyproject_path="pyproject.toml"):
+    """
+    Parse a pyproject.toml file and create a Modal image with the dependencies.
+    
+    Args:
+        pyproject_path (str): Path to the pyproject.toml file
+        
+    Returns:
+        tuple: (modal.Image, modal.App) The configured Modal image and app
+    """
+    with open(pyproject_path, "rb") as f:
+        pyproject_data = tomli.load(f)
+    
+    project_info = pyproject_data.get("project", {})
+    project_name = project_info.get("name", "default_app")
+    dependencies = project_info.get("dependencies", [])
+    
+    app = modal.App(project_name)
+    
+    image = modal.Image.debian_slim()
+    
+    if dependencies:
+        image = image.pip_install(*dependencies)
+    
+    print(f"Created Modal image for '{project_name}' with {len(dependencies)} dependencies")
+    
+    return image, app
+
+image, app = create_modal_image_from_pyproject()
